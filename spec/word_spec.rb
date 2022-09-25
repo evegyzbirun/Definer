7e9058a398c5e985f75a91995a04d1c531fb09f3
evegyzbirun <zbirunzbirun@gmail.com>
2022-09-24 21:33:52 -0700

    added all and save tests in word_spec.

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -5,12 +5,22 @@ describe '#Word' do
   
   before(:each) do
     Word.clear()
+    # word1 = Word.new("song", nil)
+    # word2 = Word.new("world", nil)
   end
   describe('.all') do
-    it('if there is no words returns an empty array') do
+    it("if there is no words returns an empty array") do
       expect(Word.all).to(eq[])
     end
   end
 
-  
+  describe('#save') do
+   it("saves a word") do
+    word1 = Word.new("song", nil)
+    word1 = save()
+    word2 = Word.new("world", nil)
+    word2 = save()
+    expect(Word.all).to(eq([word1, word2]))
+   end
+  end
 end
