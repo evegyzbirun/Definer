b9c0cfdc1ee3ccc7f95c87486ffb2e8b731183e8
evegyzbirun <zbirunzbirun@gmail.com>
2022-09-24 22:24:10 -0700

    added sort in sefl.all  word.rb.

--- lib/word.rb
+++ lib/word.rb
@@ -4,6 +4,12 @@ class Word
   @@words = {}
   @@total_rows = 0
 
-  def initialize
+  def initialize(word_input, id)
+    @word_input = word_input
+    @id = id || @@total_rows += 1
+  end
 
+  def self.all
+    @@word_input.value().sort {|a, b| a.word_input.dowcase <=> b.word_input.dowcase}
+  end
 end
