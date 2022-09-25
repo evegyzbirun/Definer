197062492a6d4cb1a9117b3c31d33bd2fef2a081
evegyzbirun <zbirunzbirun@gmail.com>
2022-09-24 22:48:32 -0700

    added self.clear in sefl.all  word.rb.

--- lib/word.rb
+++ lib/word.rb
@@ -12,4 +12,19 @@ class Word
   def self.all
     @@word_input.value().sort {|a, b| a.word_input.dowcase <=> b.word_input.dowcase}
   end
+
+  def save 
+    @@word_input[self.id] = Word.new(self.word_input, self.id)
+  end
+
+  def ==(word_to_compare)
+    self.word_input() == word_to_compare.word_input()
+  end
+
+  def self.clear
+    @@word_input = {}
+    @@total_rows = 0
+  end
+
+  def 
 end
