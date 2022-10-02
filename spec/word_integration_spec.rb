72ae34fdbada0c5f43c0d839282dde31b9b4a09b
evegyzbirun <zbirunzbirun@gmail.com>
2022-10-01 23:21:49 -0700

    added tests: create a word path and a definition path, both passed

--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -1,14 +1,25 @@
-#  require('capybara/rspec')
-#  require('./app')
-#  Capybara.app = Sinatra::Application
-#  set(:show_exceptions, false)
+ require('capybara/rspec')
+ require('./app')
+ Capybara.app = Sinatra::Application
+ set(:show_exceptions, false)
 
-#  describe('create a aword path', {:type => :feature}) do
-#   it('creates a word and then goes to the word page') do
-#     visit('/words')
-#     click_on('Add a new word')
-#     fill_in('word_name', :with => 'Yellow Submarine')
-#     click_on('Create!')
-#     expect(page).to have_content('Yellow Submarine')
-#   end
-# end
+ describe('create a word path', {:type => :feature}) do
+  it('creates a word and then goes to the word page') do
+    visit('/words')
+    click_on('Add a new word')
+    fill_in('word_name', :with => 'Yellow Submarine')
+    click_on('Create!')
+    expect(page).to have_content('Yellow Submarine')
+  end
+end
+
+describe('create a definition path', {:type => :feature}) do
+  it('creates a song and then goes to the album page') do
+    word = Word.new("Yellow", nil)
+    word.save
+    visit("/words/#{word.id}")
+    fill_in('definition_name', :with => 'sun in the moring')
+    click_on('Add definition')
+    expect(page).to have_content('sun in the moring')
+  end
+end
